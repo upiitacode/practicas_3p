c2d2e6b3b2c4d8b7fce61f18d06f44df0895b67b
Manuel Limones <limonesu.me@gmail.com>
2015-02-27 12:35:58 -0600

    Fix bugs and complete example

--- semaforo_01/source/main.c
+++ semaforo_01/source/main.c
@@ -14,9 +14,9 @@ osThreadId consumerID;
 osThreadDef(producer,osPriorityNormal,1,0);
 osThreadDef(consumer,osPriorityNormal,1,0);
 
-#define signal_mutex_not_z (0x1<<2)
+#define signal_mutex_not_z (0x1<<0)
 #define signal_empty_not_z (0x1<<1)
-#define signal_full_not_z  (0x1<<3)
+#define signal_full_not_z  (0x1<<2)
 
 //definicion del Semaphore
 osMutexDef(SM_mutex);
@@ -27,7 +27,7 @@ osMutexDef(SM_full);
 osMutexId SM_full_Id;
 
 /*Codigo del ejemplo de mutex*/
-#define N 100 /*numero  de ranuras en el buffer*/
+#define N 20 /*numero  de ranuras en el buffer*/
 typedef struct{
 	int counter;
 	int signalN;
@@ -39,9 +39,12 @@ semaphore mutex = {1,signal_mutex_not_z,&SM_mutex_Id,NULL};
 semaphore empty = {N,signal_empty_not_z,&SM_empty_Id,NULL};
 semaphore full = {0,signal_full_not_z,&SM_full_Id,NULL};
 
+
 int main(){
 	UART0_init();
+	printf("\nkernel init");
 	osKernelInitialize();
+	printf("\t[ready]\n");
 	//creamos MUTEX
 	SM_mutex_Id=osMutexCreate(osMutex(SM_mutex));
 	SM_empty_Id=osMutexCreate(osMutex(SM_empty));
@@ -63,7 +66,7 @@ int main(){
 int produce_item(void);
 void consume_item(int item);
 /*semaphore control functions*/
-void down(semaphore*,osThreadId);
+void down(semaphore*,osThreadId*);
 void up(semaphore*);
 /*buffer fucntions*/
 void enter_item(int*);
@@ -74,8 +77,8 @@ void producer(void const * args){
 	int item;
 	while(1){
 		item=produce_item();
-		down(&empty,producerID);
-		down(&mutex,producerID);
+		down(&empty,&producerID);
+		down(&mutex,&producerID);
 		enter_item(&item);
 		up(&mutex);
 		up(&full);
@@ -85,8 +88,8 @@ void producer(void const * args){
 void consumer(void const * args){
 	int item;
 	while(1){
-		down(&full,consumerID);
-		down(&mutex,consumerID);
+		down(&full,&consumerID);
+		down(&mutex,&consumerID);
 		remove_item(&item);
 		up(&mutex);
 		up(&empty);
@@ -94,12 +97,13 @@ void consumer(void const * args){
 	}
 }
 
-void down(semaphore *pSemaphore,osThreadId callerId){
+void down(semaphore *pSemaphore,osThreadId *p_callerId){
 	osMutexWait(*(pSemaphore->pMutex),osWaitForever);//wait for mutex on this semaphore
-	while((pSemaphore->counter<=0)){
-		*(pSemaphore->pSleepingThread)=callerId;//Save treadId so this thread recives the wakeup signal 
+	if((pSemaphore->counter<=0)){
+		pSemaphore->pSleepingThread=p_callerId;//Save treadId so this thread recives the wakeup signal 
 		osMutexRelease(*(pSemaphore->pMutex));//release mutex to avoid deadlock
 		osSignalWait(pSemaphore->signalN,osWaitForever);//wait  for wakeup signal
+		osSignalClear(*p_callerId,pSemaphore->signalN);
 		osMutexWait(*(pSemaphore->pMutex),osWaitForever);//wait for mutex on this semaphore
 	}
 	pSemaphore->counter--;//decrease coutner
@@ -115,18 +119,31 @@ void up(semaphore *pSemaphore){
 	osMutexRelease(*(pSemaphore->pMutex));//release mutex
 }
 
-void enter_item(int *pItem){
+int buffer_array[N];
+int index_head=0;
+int index_tail=0;
 
+void enter_item(int *pItem){
+	buffer_array[index_head]= *pItem;
+	index_head++;
+	index_head%=N;
 }
 
 void remove_item(int *pItem){
-
+	*pItem=buffer_array[index_tail];
+	index_tail++;
+	index_tail%=N;
 }
 
+int mycounter=0;
 int produce_item(void){
-	return 0;
+	mycounter++;
+	if((mycounter%25)==0){
+		osDelay(1000);
+	}
+	return mycounter;
 }
 
 void consume_item(int item){
-
+	printf("i=%d\n",item);
 }
